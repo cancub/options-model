447fc8c779714352fed8f9c0c4bccb374d09330e
AOK <alf@okenney.ca>
2020-07-03 12:01:57 -0400

    Move to bid and ask price for config.py, add extra info

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 STORAGE_DIR     = 'pickles'
 META_COLUMNS    = ['symbolId', 'type', 'strike']
-DATA_NAMES      = ['lastTradePrice', 'volume', 'volatility', 'delta', 'gamma',
+# DATA_NAMES      = ['lastTradePrice', 'volume', 'volatility', 'delta', 'gamma',
+#                    'theta', 'vega', 'rho', 'openInterest']
+DATA_NAMES      = ['bidPrice', 'askPrice', 'volume', 'volatility', 'delta', 'gamma',
                    'theta', 'vega', 'rho', 'openInterest']
 MULTITHREADED   = True
 QUIET           = True
+
+'''
+NOTE:
+when reading the numpy files:
+[datatype, timepoint, optionID]
+'''
