6521b12820fb7c249dd9b4cb0f0cbdef2e584265
AOK <alf@okenney.ca>
2020-09-18 21:03:15 -0400

    Fix bogus winner calculation

--- utils.py
+++ utils.py
@@ -279,7 +279,7 @@ def collect_winners_and_hard_losers(trades_df,
     profit_less_fees = trades_df.max_profit - calculate_fee()
 
     losers = trades_df[profit_less_fees < winning_profit]
-    winners = trades_df[profit_less_fees >= 0]
+    winners = trades_df[profit_less_fees >= winning_profit]
     total_winners = len(winners)
     total_losers = len(losers)
 
