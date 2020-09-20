338f58db3e23e186d7f88c620e7f9483dce8443a
AOK <alf@okenney.ca>
2020-09-20 16:34:36 -0400

    Give spread_worker() a better name
    We're going to be adding other spread workers soon (e.g., butterfly) so
    it needs to be given a better name. Also clean up unused code, comments

--- trade_processing.py
+++ trade_processing.py
@@ -19,7 +19,7 @@ def collect_TA(ticker, dates):
 
 # ============================== Vertical spreads ==============================
 
-def spread_worker(
+def call_put_spread_worker(
     id,
     option_type_df,
     bid_df,
@@ -29,19 +29,6 @@ def spread_worker(
     max_margin=None,
     verbose=False
 ):
-    '''
-    A thread-safe worker which takes care of collecting profits DataFrames for
-    one of the standard bull/bear call/put spreads.
-
-    The trick here is to provide only the bid_df and ask_df specific to the type
-    of option (i.e., call or put) used by the spread.
-
-    Most importantl though is the get_sell_strikes() function, which does the magic of
-    differentiation between the types of spreads. This worker will be walking
-    Through all of the strikes it can for the leg we will _buy_ and will then
-    rely on get_sell_strikes() to return the list of strikes to be used for the leg we
-    will _sell_.
-    '''
     hdr = 'COLLECTOR {:>2}:'.format(id)
     if verbose:
         print('{} START'.format(hdr))
@@ -290,7 +277,6 @@ def collect_spreads(
     ticker,
     expiry,
     options_df,
-    vertical=True,
     getter_procs=5,
     saver_procs=5,
     max_margin=config.MARGIN,
@@ -357,7 +343,7 @@ def collect_spreads(
             processes = []
             for i in range(getter_procs):
                 p = multiprocessing.Process(
-                    target=spread_worker,
+                    target=call_put_spread_worker,
                     args=(i,
                           option_type_df,
                           bid_df,
@@ -394,7 +380,7 @@ def collect_spreads(
                 p.join()
 
         else:
-            spread_worker(
+            call_put_spread_worker(
                 0,
                 option_type_df,
                 bid_df,
@@ -406,246 +392,3 @@ def collect_spreads(
             )
 
     return tmpdir
-
-def bull_bear_phase_spread(ticker):
-    '''
-    A bull-bear phase spread can be constructed using near month call & put.
-    '''
-    pass
-
-
-
-# =============================== Ratio Spreads ================================
-'''
-Our go to ratio-spread is a front-ratio spread. We normally do not route back-ratio spreads, which is where we are purchasing more options than we are selling, because this would be routed for a debit. We always prefer to collect premium and put ourselves in high probability situations. The beauty of this trade is that if we’re directionally wrong, it doesn’t matter if our spread expires OTM as long as we collect a credit - that will then be our profit. We route front-ratio spreads as a means to get into a long or short stock positon with a very beneficial breakeven point. We tend to use these strategies if we have a price target in mind for the underlying. We will usually place our short strike at that target, as that would yield max profit at expiration if the stock ends up there.
-'''
-
-def call_ratio_spread(ticker):
-    '''
-    A Call Front Ratio Spread is a neutral to bullish strategy that is created
-    by purchasing a call debit spread with an additional short call at the short
-    strike of the debit spread. The strategy is generally placed for a net
-    credit so that there is no downside risk.
-
-    Directional Assumption: Neutral to slightly bullish
-
-    Setup:
-    - Buy an ATM or OTM call option
-    - Sell two further OTM call options at a higher strike
-
-    Ideal Implied Volatility Environment : High
-
-    Max Profit: Distance between long strike and short strike + credit received
-
-    How to Calculate Breakeven(s): Short call strike + max profit potential
-    '''
-    pass
-
-def put_ratio_spread(ticker):
-    '''
-    A Put Front Ratio Spread is a neutral to bearish strategy that is created by
-    purchasing a put debit spread with an additional short put at the short
-    strike of the debit spread. The strategy is generally placed for a net
-    credit so that there is no upside risk.
-
-    Directional Assumption: Neutral to slightly bearish
-
-    Setup:
-    - Buy an ATM or OTM put option
-    - Sell two further OTM put options at a lower strike
-
-    Ideal Implied Volatility Environment : High
-
-    Max Profit: Distance between long strike and short strike + credit received
-
-    How to Calculate Breakeven(s): Short put strike - max profit potential
-    '''
-
-# ================================ Backspreads =================================
-
-def call_backspread(ticker):
-    '''
-    The call backspread (reverse call ratio spread) is a bullish strategy in
-    options trading whereby the options trader writes a number of call options
-    and buys more call options of the same underlying stock and expiration date
-    but at a higher strike price. It is an unlimited profit, limited risk
-    strategy that is used when the trader thinks that the price of the
-    underlying stock will rise sharply in the near future.
-
-    A 2:1 call backspread can be created by selling a number of calls at a lower
-    strike price and buying twice the number of calls at a higher strike. 
-    '''
-    pass
-
-def put_backspread(ticker):
-    '''
-    The put backspread is a strategy in options trading whereby the options
-    trader writes a number of put options at a higher strike price (often
-    at-the-money) and buys a greater number (often twice as many) of put options
-    at a lower strike price (often out-of-the-money) of the same underlying
-    stock and expiration date. Typically the strikes are selected such that the
-    cost of the long puts is largely offset by the premium earned in writing the
-    at-the-money puts. This strategy is generally considered very bearish but it
-    can also serve as a neutral/bullish play under the right conditions.  
-    '''
-    pass
-
-# ================================= convoluted =================================
-
-def iron_condor(ticker):
-    '''
-    An Iron Condor is a directionally neutral, defined risk strategy that
-    profits from a stock trading in a range through the expiration of the
-    options. It benefits from the passage of time and any decreases in implied
-    volatility.
-
-    Directional Assumption: Neutral
-
-    Setup:
-    - Sell OTM Call Vertical Spread
-    - Sell OTM Put Vertical Spread
-    '''
-    pass
-
-def strangle(ticker):
-    '''
-    A short strangle is a position that is a neutral strategy that profits when the stock stays between the short strikes as time passes, as well as any decreases in implied volatility. The short strangle is an undefined risk option strategy.
-
-    Directional Assumption: Neutral
-
-    Setup:
-    - Sell OTM Call
-    - Sell OTM Put
-
-    Ideal Implied Volatility Environment : High
-
-    Max Profit: Credit received from opening trade
-
-    How to Calculate Breakeven(s):
-    - Downside: Subtract total credit from short put
-    - Upside: Add total credit to short call 
-    '''
-    pass
-
-def straddle(ticker):
-    '''
-    A short straddle is a position that is a neutral strategy that profits from
-    the passage of time and any decreases in implied volatility. The short
-    straddle is an undefined risk option strategy.
-
-    Directional Assumption: Neutral
-
-    Setup:
-    - Sell ATM Call
-    - Sell ATM Put
-
-    Ideal Implied Volatility Environment : High
-
-    Max Profit: Credit received from opening trade
-
-    How to Calculate Breakeven(s):
-    - Downside: Subtract initial credit from Put strike price
-    - Upside: Add initial credit to the Call strike price
-    '''
-    pass
-
-def butterfly_spread(ticker):
-    '''
-    A long butterfly spread is a neutral position that’s used when a trader
-    believes that the price of an underlying is going to stay within a
-    relatively tight range.
-
-    Directional Assumption: Neutral
-
-    Setup: This spread is typically created using a ratio of 1-2-1
-    (1 ITM option, 2 ATM options, 1 OTM option).
-    - Buy Call/Put (above short strike)
-    - Sell 2 Calls/Puts
-    - Buy Call/Put (below short strike)
-
-    Ideal Implied Volatility Environment : High
-
-    Max Profit:
-    The distance between the short strike and long strike, less the debit paid.
-
-    How to Calculate Breakeven(s):
-    - Upside: Higher Long Option Strike - Debit Paid
-    - Downside: Lower Long Option Strike + Debit Paid
-    '''
-    pass
-
-def broken_wing_butterfly(ticker):
-    '''
-    A Broken Wing Butterfly is a long butterfly spread with long strikes that
-    are not equidistant from the short strike. This leads to one side having
-    greater risk than the other, which makes the trade slightly more directional
-    than a standard long butterfly spread.
-
-    Directional Assumption: Neutral / Slightly Directional
-
-    Setup: Broken wing butterfly spreads can be constructed with either all
-    calls or all puts. The trade is comprised of two short options and a long
-    option above and below the short strike:
-    - Buy Call/Put (above short strike)
-    - Sell 2 Calls/Puts
-    - Buy Call/Put (below short strike)
-    '''
-    pass
-
-def calendar_spread(ticker):
-    '''
-    A Long Calendar Spread is a low-risk, directionally neutral strategy that
-    profits from the passage of time and/or an increase in implied volatility.
-
-    Directional Assumption: Neutral
-
-    Setup: A calendar is comprised of a short option (call or put) in a
-    near-term expiration cycle, and a long option (call or put) in a longer-term
-    expiration cycle. Both options are of the same type and use the same strike
-    price.
-
-    - Sell near-term Put/Call
-    - Buy longer-term Put/Call
-    '''
-    pass
-
-def iron_fly(ticker):
-    '''
-    An Iron Fly is essentially an Iron Condor with call and put credit spreads
-    that share the same short strike. This creates a very neutral position that
-    profits from the passage of time and any decreases in implied volatility. An
-    Iron Fly is synthetically the same as a long butterfly spread using the same
-    strikes.
-
-    Directional Assumption: Neutral
-
-    Setup:
-    - Buy OTM Put option
-    - Sell Straddle (short call and short put at the same strike, typically
-      At-The-Money)
-    - Buy OTM Call option
-    '''
-    pass
-
-def jade_lizard(ticker):
-    '''
-    A Jade Lizard is a slightly bullish strategy that combines a short put and a
-    short call spread. The strategy is created to have no upside risk, which is
-    done by collecting a total credit greater than the width of the short call
-    spread.
-
-    Directional Assumption: Neutral / Bullish
-
-    Setup:
-    - Sell OTM Put
-    - Sell OTM Vertical Call Spread
-
-    Ideal Implied Volatility Environment : High
-
-    Max Profit: Credit received from opening trade. Max profit is realized when
-    the stock price is between the short strikes at expiration.
-
-    How to Calculate Breakeven(s):
-    - Downside: Strike Price of short put - credit received 
-    '''
-    pass
